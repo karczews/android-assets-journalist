87e2010913bc3adeebc6b55bad23df46c5fa6dc3
Krzysztof Karczewski <kr.karczewski@gmail.com>
2019-03-13 23:44:34 +0100

    Update project license due to rename

--- src/main/kotlin/com/github/utilx/assetsjournalist/java/JavaFileConfig.kt
+++ src/main/kotlin/com/github/utilx/assetsjournalist/java/JavaFileConfig.kt
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2019-present, Android Asset File Generator Contributors.
+ *  Copyright (c) 2019-present, Android Assets Journalist Contributors.
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  *  compliance with the License. You may obtain a copy of the License at
  *
