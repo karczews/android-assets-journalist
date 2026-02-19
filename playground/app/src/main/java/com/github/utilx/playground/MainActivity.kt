09bfc3ab42535c43c60601f21703b22346378937
Krzysztof Karczewski <kr.karczewski@gmail.com>
2026-02-19 22:46:33 +0100

    test: remove Java asset file checks from playground

--- playground/app/src/main/java/com/github/utilx/playground/MainActivity.kt
+++ playground/app/src/main/java/com/github/utilx/playground/MainActivity.kt
@@ -14,9 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.d("TEST", getString(R.string.prefix_az_123_txt_1028221614))
 
-        Log.d(TAG, "Java asset file smoketest 1 ${AssetFilesJava.CONSTPREFIXJAVA_REPLACEJAVA_123_TXT_2038126485}")
-        Log.d(TAG, "Java asset file smoketest 2 ${AssetFilesJava.CONSTPREFIXJAVA_ROOT_JAVA_TXT_1351089311}")
-
         Log.d(TAG, "Kotlin asset file smoketest 1 ${AssetFilesKotlin.CONSTPREFIXKT_REPLACEKT_123_TXT_371122798}")
         Log.d(TAG, "Kotlin asset file smoketest 2 ${AssetFilesKotlin.CONSTPREFIXKT_ROOT_KT_TXT_203164056}")
 
